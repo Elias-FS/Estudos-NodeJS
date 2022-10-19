6cebc7b2aa83e6657c2dddf180a6f4a1d82b6ea9
Anubz <elias-fausto@hotmail.com>
2022-10-19 16:56:38 -0300

    att codigo

--- FINAPI/src/index.js
+++ FINAPI/src/index.js
@@ -3,6 +3,8 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
 const customers = [];
 
 /**
@@ -13,12 +15,16 @@ const customers = [];
  */
 
 app.post("/account", (request, response) => {
-  const { cpf, name } = requests.body;
+  const { cpf, name } = request.body;
 
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({error: "Customer already exists"});
+  }
 
   customers.push({
-    id,
+    id: uuidv4(),
     cpf,
     name,
     statment: [],
@@ -27,5 +33,17 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 
 });
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(404).json({error: 'Customer not found'});
+  }
+
+  return response.json(customer.statment);
+})
 
 app.listen(3333);
