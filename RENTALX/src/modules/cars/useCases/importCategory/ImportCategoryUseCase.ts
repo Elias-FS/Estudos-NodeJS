01505005ef0f065d8570b874c20ebd370ceab7a8
Anubz <elias-fausto@hotmail.com>
2022-12-07 14:25:50 -0300

    Att estudos

--- RENTALX/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ RENTALX/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -22,7 +22,6 @@ class ImportCategoryUseCase {
 
             parseFile
                 .on("data", async (line) => {
-                    // ["name", "description"]
                     const [name, description] = line;
                     categories.push({
                         name,
@@ -30,6 +29,7 @@ class ImportCategoryUseCase {
                     });
                 })
                 .on("end", () => {
+                    fs.promises.unlink(file.path);
                     resolve(categories);
                 })
                 .on("error", (err) => {
